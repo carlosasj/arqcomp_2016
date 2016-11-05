49dc158301d99a49fd928505625a088cf60a24ca
Carlos Schneider <carlosaschjr@gmail.com>
2016-11-05 19:08:21 -0200

    Correções

--- src/services/ula.js
+++ src/services/ula.js
@@ -2,29 +2,20 @@ angular.module('arqcompApp').factory('ULA', ['Registers', function (Registers) {
     var output = 0;
 
     var functions = {
-        addi : (val1, val2) => {
-            return Registers.get(val1) + val2;
-        },
         add : (val1, val2) => {
-            return Registers.get(val1) + Registers.get(val2);
-        },
-        subi : (val1, val2) => {
-            return Registers.get(val1) - val2;
+            return val1 + val2;
         },
         sub : (val1, val2) => {
-            return Registers.get(val1) - Registers.get(val2);
-        },
-        muli : (val1, val2) => {
-            return Registers.get(val1) * val2;
+            return val1 - val2;
         },
         mul : (val1, val2) => {
-            return Registers.get(val1) * Registers.get(val2);
+            return val1 * val2;
         },
         cmp : (val1, val2) => {
             return val1 - val2;
         },
-        mov : (val1, val2) => {
-            return Registers.get(val1);
+        mov : (val1) => {
+            return val1;
         }
     };
 
