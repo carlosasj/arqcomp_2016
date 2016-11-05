88bc8d2d2c1ce602982d669c720d5cac262a2fdc
Carlos Schneider <carlosaschjr@gmail.com>
2016-11-05 19:15:54 -0200

    Correções 2

--- src/services/ula.js
+++ src/services/ula.js
@@ -1,4 +1,8 @@
 angular.module('arqcompApp').factory('ULA', ['Registers', function (Registers) {
+    var val1 = 0;
+    var val2 = 0;
+    var function_name = 'noop';
+
     var output = 0;
 
     var functions = {
@@ -16,15 +20,25 @@ angular.module('arqcompApp').factory('ULA', ['Registers', function (Registers) {
         },
         mov : (val1) => {
             return val1;
-        }
+        },
+        noop : (val1) => {
+            return 0;
+        },
     };
 
-    var execute = (func_name, val1, val2) => {
-        output = functions[func_name](val1, val2)
+    var set_func = func => {function_name = func};
+    var set_val1 = val => {val1 = val};
+    var set_val2 = val => {val2 = val};
+
+    var execute = () => {
+        output = functions[function_name](val1, val2)
     };
 
     return {
         output: output,
         execute: execute,
+        set_func: set_func,
+        set_val1: set_val1,
+        set_val2: set_val2,
     }
 }]);
