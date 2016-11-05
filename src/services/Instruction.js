64755f8b867e804e2470c5cebb89c501fe68ffe2
André Missaglia <andre.missaglia@gmail.com>
2016-11-05 19:16:52 -0200

    Adds NOP.

--- src/services/Instruction.js
+++ src/services/Instruction.js
@@ -4,6 +4,7 @@ angular.module('arqcompApp').factory('Instruction', [function () {
             'ADDI $r2 $0 1',//f[i-1]
             'ADDI $r3 $0 1',//f[i]
             'ADDI $r4 $0 2',//i
+            'NOP',//RAW
             'CMP $r4 $r1',
             'JE 40',
             'ADD $r5 $r2 $r3',//f[i+1] = f[i] + f[i-1]
