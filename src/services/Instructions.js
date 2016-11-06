1b40dc9dde7ee1725ea5aa25d1c839299352585d
André Missaglia <andre.missaglia@gmail.com>
2016-11-05 22:19:53 -0200

    Title for the codes.

--- src/services/Instructions.js
+++ src/services/Instructions.js
@@ -1,24 +1,27 @@
 angular.module('arqcompApp').factory('Instructions', [function () {
     var predefinedCodes = [
-        [
-            'ADDI $r2 $0 1',//f[i-1]
-            'ADDI $r3 $0 1',//f[i]
-            'ADDI $r4 $0 2',//i
-            'NOP',//RAW
-            'CMP $r4 $r1',
-            'JE 40',
-            'ADD $r5 $r2 $r3',//f[i+1] = f[i] + f[i-1]
-            'MOV $r2 $r3',
-            'MOV $r3 $r5',
-            'ADDI $r4 $r4 1',
-            'JMP 12',
-            'HALT'
-        ]
+        {
+            title: 'Fibonacci',
+            code: [
+                'ADDI $r2 $0 1',//f[i-1]
+                'ADDI $r3 $0 1',//f[i]
+                'ADDI $r4 $0 2',//i
+                'NOP',//RAW
+                'CMP $r4 $r1',
+                'JE 40',
+                'ADD $r5 $r2 $r3',//f[i+1] = f[i] + f[i-1]
+                'MOV $r2 $r3',
+                'MOV $r3 $r5',
+                'ADDI $r4 $r4 1',
+                'JMP 12',
+                'HALT'
+            ]
+        },
     ];
-    var code = predefinedCodes[0];
+    var code = predefinedCodes[0].code;
     return {
         getCode: codeNumer => {
-            code = predefinedCodes[codeNumber];
+            code = predefinedCodes[codeNumber].code;
         },
         get: pc => {
             pc /= 4;
