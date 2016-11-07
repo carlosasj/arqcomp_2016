94be59e5dd3cee7e199e47ebe4c83bc211e743f7
Carlos Alberto Schneider Júnior <carlosaschjr@gmail.com>
2016-11-06 22:06:24 -0200

    Add files via upload

--- app.js
+++ app.js
@@ -40289,56 +40289,6 @@ Picker.extend( 'pickadate', DatePicker )
     }; // Plugin end
 }( jQuery ));
 angular.module('arqcompApp', ['ngSanitize']);
-angular.module("arqcompApp").controller('ConfigCtrl', ['$scope', '$rootScope', 'Instructions', function ($scope, $rootScope, Instructions) {
-    $rootScope.$broadcast('reset');
-    $scope.instructions_db = Instructions.predefinedCodes;
-
-    if ($rootScope.asm_config) {
-        $scope.form = $rootScope.asm_config;
-    } else {
-        $scope.form = {
-            selected_code: Instructions.predefinedCodes[0],
-            prediction_type: 0,
-            number_of_bits: 1,
-        };
-    }
-
-    $scope.error = '';
-
-    var validate = (form) => {
-        if (typeof form.selected_code == 'undefined') return 'selected_code_not_defined';
-        if (typeof form.prediction_type == 'undefined') return 'prediction_type_not_defined';
-        if (form.prediction_type == 2 && !form.number_of_bits >= 1 && !form.number_of_bits <= 8) return 'number_of_bits_not_in_range';
-        if (form.prediction_type < 0 || form.prediction_type > 2) return 'prediction_type_not_in_range';
-
-        return true;
-    };
-
-    $scope.validate_and_change_tab = (form) => {
-        var valid = validate(form);
-        if (valid == true) {
-            $rootScope.asm_config = form;
-            $rootScope.$broadcast('change-tab-to', 'exec');
-        } else {
-            $scope.error = valid;
-        }
-    };
-
-    setTimeout(Materialize.updateTextFields, 10);
-}]);
-angular.module("arqcompApp").controller('ExecCtrl', ['$scope', '$rootScope', 'Instructions', function ($scope, $rootScope, Instructions) {
-    $scope.back_to_config = () => {
-        $rootScope.$broadcast('change-tab-to', 'config');
-    };
-}]);
-angular.module("arqcompApp").controller('MainCtrl', ['$scope', 'Instructions', function ($scope, Instructions) {
-    $scope.tab = 'config';
-
-    $scope.$on('change-tab-to', (event, arg) => {
-        $scope.tab = arg;
-    });
-
-}]);
 angular.module('arqcompApp').filter('syntax', ['$sce', $sce => {
 
     var wrapper = (txt, regex, prefix, sufix) => {
@@ -40395,6 +40345,56 @@ angular.module('arqcompApp').filter('syntax', ['$sce', $sce => {
 }]);
 
 
+angular.module("arqcompApp").controller('ConfigCtrl', ['$scope', '$rootScope', 'Instructions', function ($scope, $rootScope, Instructions) {
+    $rootScope.$broadcast('reset');
+    $scope.instructions_db = Instructions.predefinedCodes;
+
+    if ($rootScope.asm_config) {
+        $scope.form = $rootScope.asm_config;
+    } else {
+        $scope.form = {
+            selected_code: Instructions.predefinedCodes[0],
+            prediction_type: 0,
+            number_of_bits: 1,
+        };
+    }
+
+    $scope.error = '';
+
+    var validate = (form) => {
+        if (typeof form.selected_code == 'undefined') return 'selected_code_not_defined';
+        if (typeof form.prediction_type == 'undefined') return 'prediction_type_not_defined';
+        if (form.prediction_type == 2 && !form.number_of_bits >= 1 && !form.number_of_bits <= 8) return 'number_of_bits_not_in_range';
+        if (form.prediction_type < 0 || form.prediction_type > 2) return 'prediction_type_not_in_range';
+
+        return true;
+    };
+
+    $scope.validate_and_change_tab = (form) => {
+        var valid = validate(form);
+        if (valid == true) {
+            $rootScope.asm_config = form;
+            $rootScope.$broadcast('change-tab-to', 'exec');
+        } else {
+            $scope.error = valid;
+        }
+    };
+
+    setTimeout(Materialize.updateTextFields, 10);
+}]);
+angular.module("arqcompApp").controller('ExecCtrl', ['$scope', '$rootScope', 'Instructions', function ($scope, $rootScope, Instructions) {
+    $scope.back_to_config = () => {
+        $rootScope.$broadcast('change-tab-to', 'config');
+    };
+}]);
+angular.module("arqcompApp").controller('MainCtrl', ['$scope', 'Instructions', function ($scope, Instructions) {
+    $scope.tab = 'config';
+
+    $scope.$on('change-tab-to', (event, arg) => {
+        $scope.tab = arg;
+    });
+
+}]);
 angular.module('arqcompApp').directive('debugcpuDirective', [function() {
 	return {
 		restrict: 'AE',
